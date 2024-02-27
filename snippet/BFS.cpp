1ae0ff9d1e0b792de15692eb65e3bfacca24dac9
안성원 <ans2568@gmail.com>
2024-02-27 12:00:00 +0000

    BFS 알고리즘 스니펫
    - 2차원 그리드 유형 문제 스니펫

--- snippet/BFS.cpp
+++ snippet/BFS.cpp
@@ -8,6 +8,13 @@ void BFS(int x); // 무방향 그래프(노드)
 vector<vector<int>> node_graph;
 vector<bool> visited;
 
+void BFS(int x, int y); // WxH 로 나타내는 그리드 행렬
+int W, H;               // width, height
+int dist[101][101];     // distance
+int graph[101][101];
+vector<vector<bool>> isVisited;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
 void BFS(int x) {
   queue<int> queue;
@@ -21,6 +28,28 @@ void BFS(int x) {
       if (visited[node_graph[now][i]] == false) {
         visited[node_graph[now][i]] = true;
         queue.push(node_graph[now][i]);
+      }
+    }
+  }
+}
+
+void BFS(int x, int y) {
+  dist[x][y] = 1;
+  queue<pair<int, int>> queue;
+  queue.push(make_pair(x, y));
+  while (!queue.empty()) {
+    int now_x = queue.front().first;
+    int now_y = queue.front().second;
+    queue.pop();
+    for (int i = 0; i < 4; i++) {
+      int nx = now_x + dx[i];
+      int ny = now_y + dy[i];
+      if (nx >= 1 && ny >= 1 && nx <= W && ny <= H) {
+        if (graph[nx][ny] == 1 && !isVisited[nx][ny]) {
+          isVisited[nx][ny] = true;
+          queue.push(make_pair(nx, ny));
+          dist[nx][ny] = dist[now_x][now_y] + 1;
+        }
       }
     }
   }
