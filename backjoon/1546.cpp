c232ae1fa2bbb914c7eb99d5939d204468561f37
안성원 <ans2568@gmail.com>
2024-03-11 03:54:24 +0000

    백준 1546번 문제
    - 1차원 배열

--- backjoon/1546.cpp
+++ backjoon/1546.cpp
@@ -4,33 +4,29 @@
 
 using namespace std;
 
+int N;
+vector<double> score;
+
 int main() {
-  int N;
   cin >> N;
-  int *scores = new int[N];
-  // 입력
-  for (int i = 0; i < N; i++) {
-    cin >> scores[i];
+  score.resize(N);
+  int max_score = 0;
+  for (int i=0; i<N; i++) {
+    double num;
+    cin >> num;
+    if (num > max_score)
+        max_score = num;
+    score[i] = num;
   }
 
-  vector<int> score_vec;
-  for (int i = 0; i < N; i++)
-    score_vec.push_back(scores[i]);
-  // 최댓값 찾기
-  auto max_ = max_element(score_vec.begin(), score_vec.end());
-  vector<double> new_scores;
-  // 점수 치환
-  for (auto &s : score_vec) {
-    double new_s = (static_cast<double>(s) / *max_.base()) * 100.0;
-    new_scores.push_back(new_s);
-  }
-  // 평균 구하기
   double sum = 0;
-  for (auto &ns : new_scores) {
-    sum += ns;
+  for (int i=0; i<N; i++) {
+    score[i] = score[i] / max_score * 100;
+    sum += score[i];
   }
-  cout.precision(8);
-  cout << sum / N << endl;
+
+  double avg = sum / N;
+  cout << avg << endl;
 
   return 0;
 }
