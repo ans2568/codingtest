684ad4a22f82d9c857163a0d27e9e9a55bbc9175
ans2568 <ans2568@naver.com>
2024-03-25 07:24:16 +0000

    백준 2178번 문제 재풀이
    - bfs

--- backjoon/2178.cpp
+++ backjoon/2178.cpp
@@ -1,54 +1,46 @@
 #include <iostream>
 #include <queue>
 #include <string>
-#include <vector>
-
 using namespace std;
-int N, M;
-int graph[101][101];
-void bfs();
-queue<pair<int, int>> queue_;
+
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-vector<vector<bool>> visited;
-vector<vector<int>> dist;
+int N, M;
+int graph[101][101];
+bool visited[101][101] = {false};
+int dist[101][101];
+
+void bfs() {
+  queue<pair<int, int>> q;
+  q.push(make_pair(1, 1));
+  dist[1][1] = 1;
+  visited[1][1] = true;
+  while (!q.empty()) {
+    int x = q.front().first;
+    int y = q.front().second;
+    q.pop();
+    for (int i = 0; i < 4; i++) {
+      int new_x = x + dx[i];
+      int new_y = y + dy[i];
+      if ((1 <= new_x && new_x <= N) && (1 <= new_y && new_y <= M) &&
+          visited[new_x][new_y] == false && graph[new_x][new_y] == 1) {
+        visited[new_x][new_y] = true;
+        q.push(make_pair(new_x, new_y));
+        dist[new_x][new_y] = dist[x][y] + 1;
+      }
+    }
+  }
+}
 
 int main() {
   cin >> N >> M;
-  visited.resize(N + 1, vector<bool>(M + 1, false));
-  dist.resize(N+1, vector<int>(M+1));
   for (int i = 1; i <= N; i++) {
-    string num;
-    cin >> num;
-    for (int j = 1; j <= M; j++) {
-      int one = num[j - 1] - '0';
-      if (one) {
-        graph[i][j] = one;
-      }
+    string str;
+    cin >> str;
+    for (int j = 0; j < str.size(); j++) {
+      graph[i][j + 1] = str[j] - '0';
     }
   }
   bfs();
   cout << dist[N][M] << endl;
-  return 0;
-}
-
-void bfs() {
-  dist[1][1] = 1;
-  visited[1][1] = true;
-  queue_.push(make_pair(1, 1));
-  while (!queue_.empty()) {
-    auto now = queue_.front();
-    queue_.pop();
-    for (int i = 0; i < 4; i++) {
-      if (now.first + dx[i] >= 1 && now.second + dy[i] >= 1 &&
-          now.first + dx[i] <= N && now.second + dy[i] <= M) {
-        if (graph[now.first + dx[i]][now.second + dy[i]] &&
-            !visited[now.first + dx[i]][now.second + dy[i]]) {
-          visited[now.first + dx[i]][now.second + dy[i]] = true;
-          queue_.push(make_pair(now.first + dx[i], now.second + dy[i]));
-          dist[now.first + dx[i]][now.second + dy[i]] = dist[now.first][now.second] + 1;
-        }
-      }
-    }
-  }
 }
