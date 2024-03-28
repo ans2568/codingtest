c5f077a3413bcf8a564ce4c9f0b36ea352eb1dcd
ans2568 <ans2568@naver.com>
2024-03-28 09:56:09 +0000

    백준 11655번 문제
    - 알고리즘 기초(ROT13)

---
+++ backjoon/11655.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str;
+  getline(cin, str);
+  for (int i = 0; i < str.size(); ++i) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+        if ((str[i] - 'a' < 13))
+        str[i] += 13;
+        else
+        str[i] -= 13;
+    } else if ((str[i] >= 'A' && str[i] <= 'Z')) {
+      if (str[i] - 'A' < 13)
+        str[i] += 13;
+      else
+        str[i] -= 13;
+    }
+    cout << str[i];
+  }
+  cout << endl;
+}
