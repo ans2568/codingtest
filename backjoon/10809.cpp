702cdc22c8bed47f849dec40a15921591b500d0b
ans2568 <ans2568@naver.com>
2024-03-28 08:29:45 +0000

    백준 10809번 문제
    - 알고리즘 기초(알파벳 찾기)

--- backjoon/10809.cpp
+++ backjoon/10809.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <cstring>
 using namespace std;
 
-string str;
-vector<pair<int, char>> alphabet = {
-    {1, 'a'}, {2, 'b'}, {3, 'c'},  {4, 'd'},  {5, 'e'},  {6, 'f'},  {7, 'g'},
-    {8, 'h'}, {9, 'i'}, {10, 'j'}, {11, 'k'}, {12, 'l'}, {13, 'm'}, {14, 'n'},
-    {15, 'o'}, {16, 'p'}, {17, 'q'}, {18, 'r'}, {19, 's'}, {20, 't'}, {21, 'u'},
-    {22, 'v'}, {23, 'w'}, {24, 'x'}, {25, 'y'}, {26, 'z'}};
-vector<int> idx(26, -1);
-
+int alphabet[26];
+bool visited[26];
 int main() {
+  string str;
   cin >> str;
-  for (int i = 0; i < str.length(); i++) {
-    for (auto c : alphabet) {
-        if (str[i] == c.second && idx[c.first - 1] == -1)
-          idx[c.first - 1] = i;
-    }
+  memset(alphabet, -1, sizeof(alphabet));
+  memset(visited, false, sizeof(visited));
+  for (int i = 0; i < str.size(); ++i) {
+    int idx = str[i] - 'a';
+    if (visited[idx] == true) continue;
+    alphabet[idx] = i;
+    visited[idx] = true;
   }
-  for (auto i : idx)
-    cout << i << " ";
+  for (int i = 0; i < 26; ++i) {
+    cout << alphabet[i] << " ";
+  }
   cout << endl;
-  return 0;
 }
