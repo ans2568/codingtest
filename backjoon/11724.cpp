3cd345858160f8a552b168facdfd7b6f1ac171fb
ans2568 <ans2568@naver.com>
2024-03-30 10:27:28 +0000

    백준 11724번 문제 재풀이
    - 알고리즘 기초(연결 요소의 개수)

--- backjoon/11724.cpp
+++ backjoon/11724.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
+#include <queue>
+#define MAX 1001
 using namespace std;
 
-void DFS(int a);
-int N, M;
-static vector<vector<int>> node;
-static vector<bool> visited;
-
-int main() {
-    cin >> N >> M;
-    // index 0은 사용하지 않으므로 배열을 하나 더 추가해서 사용
-    node.resize(N+1);
-    visited = vector<bool>(N+1, false);
-    for (int i=0; i<M; i++) {
-        int s, e;
-        cin >> s >> e;
-        // 무방향 그래프
-        node[s].push_back(e);
-        node[e].push_back(s);
+int N, M, cnt = 0;
+vector<vector<int>> graph;
+bool visited[MAX];
+void bfs(int n) {
+  if (visited[n] == true)
+    return;
+  queue<int> q;
+  q.push(n);
+  cnt++;
+  visited[n] = true;
+  while (!q.empty()) {
+    int now = q.front();
+    q.pop();
+    for (int i = 0; i < graph[now].size(); i++) {
+      if (visited[graph[now][i]] == false) {
+        visited[graph[now][i]] = true;
+        q.push(graph[now][i]);
+      }
     }
-    int cnt = 0;
-
-    for (int i=1; i<N+1; i++) {
-        if (!visited[i]) {
-            cnt++;
-            DFS(i);
-        }
-    }
-    cout << cnt << endl;
-    return 0;
+  }
 }
 
-void DFS(int num) {
-    if (visited[num])
-        return;
-    visited[num] = true;
-    for (int i : node[num]) {
-        if (visited[i] == false)
-            DFS(i);
-    }
+int main() {
+  cin >> N >> M;
+  graph.resize(N + 1);
+  for (int i = 0; i < M; i++) {
+    int u, v;
+    cin >> u >> v;
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+  }
+  for (int i = 1; i <= N; i++)
+    bfs(i);
+  cout << cnt << "\n";
 }
