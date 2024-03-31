737492859805206cea070a8903c87ce3c6a4bbcb
ans2568 <ans2568@naver.com>
2024-03-31 08:08:48 +0000

    백준 2178번 문제 재풀이
    - 알고리즘 기초(미로 탐색)

--- backjoon/2178.cpp
+++ backjoon/2178.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include <utility>
+#define MAX_SIZE 101
 using namespace std;
 
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
 int N, M;
-int graph[101][101];
-bool visited[101][101] = {false};
-int dist[101][101];
+int graph[MAX_SIZE][MAX_SIZE];
+int dist[MAX_SIZE][MAX_SIZE] = {0, };
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+bool visited[MAX_SIZE][MAX_SIZE] = {false, };
 
 void bfs() {
   queue<pair<int, int>> q;
@@ -22,8 +24,8 @@ void bfs() {
     for (int i = 0; i < 4; i++) {
       int new_x = x + dx[i];
       int new_y = y + dy[i];
-      if ((1 <= new_x && new_x <= N) && (1 <= new_y && new_y <= M) &&
-          visited[new_x][new_y] == false && graph[new_x][new_y] == 1) {
+      if ((new_x <= 0 || new_x > N) || (new_y <=0 || new_y > M)) continue;
+      if (graph[new_x][new_y] && !visited[new_x][new_y]) {
         visited[new_x][new_y] = true;
         q.push(make_pair(new_x, new_y));
         dist[new_x][new_y] = dist[x][y] + 1;
@@ -31,16 +33,15 @@ void bfs() {
     }
   }
 }
-
 int main() {
   cin >> N >> M;
   for (int i = 1; i <= N; i++) {
-    string str;
-    cin >> str;
-    for (int j = 0; j < str.size(); j++) {
-      graph[i][j + 1] = str[j] - '0';
+    string row;
+    cin >> row;
+    for (int j = 1; j <= M; j++) {
+      graph[i][j] = row[j-1] - '0';
     }
   }
   bfs();
-  cout << dist[N][M] << endl;
+  cout << dist[N][M] << "\n";
 }
